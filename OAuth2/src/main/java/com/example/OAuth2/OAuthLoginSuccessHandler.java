880e5322fd18d016d75a0ad04dec79343856e594
noorulwaheed01 <n.ul.waheed01@gmail.com>
2025-11-18 19:28:11 +0500

    configure github oauth2 client login.

---
+++ OAuth2/src/main/java/com/example/OAuth2/OAuthLoginSuccessHandler.java
@@ -0,0 +1,31 @@
+package com.example.OAuth2;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class OAuthLoginSuccessHandler implements AuthenticationSuccessHandler {
+    private static final Logger log = LoggerFactory.getLogger(OAuthLoginSuccessHandler.class);
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        log.info("onAuthenticationSuccess : {}", authentication.getPrincipal());
+
+        OAuth2AuthenticationToken  token = (OAuth2AuthenticationToken) authentication;
+
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(token.toString());
+    }
+}
---
+++ OAuth2/src/main/java/com/example/OAuth2/Security.java
@@ -0,0 +1,30 @@
+package com.example.OAuth2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class Security {
+
+    @Autowired
+    private OAuthLoginSuccessHandler oAuth2SuccessHandler;
+
+    @Bean
+    public SecurityFilterChain  securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").authenticated()
+                )
+                .oauth2Login(oauth ->
+                        oauth.successHandler(oAuth2SuccessHandler));
+        return http.build();
+    }
+
+}
